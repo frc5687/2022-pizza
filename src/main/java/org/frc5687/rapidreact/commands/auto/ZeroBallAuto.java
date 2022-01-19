a34e9b64c9048b831d771f10416c51daef13f5c6
jackellingwood <72519497+jackellingwood@users.noreply.github.com>
2022-01-19 15:09:39 -0500

    Zero-Ball initial commit
    I'm basically just committing this so I can put it on my school computer.

---
+++ src/main/java/org/frc5687/rapidreact/commands/auto/ZeroBallAuto.java
@@ -0,0 +1,19 @@
+package org.frc5687.rapidreact.commands.auto;
+
+
+import org.frc5687.rapidreact.Constants.DriveTrain;
+import org.frc5687.rapidreact.commands.DriveTrajectory;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+
+public class ZeroBallAuto extends SequentialCommandGroup{
+    public ZeroBallAuto(
+        DriveTrain driveTrain
+    ) {
+        /*addCommands(
+            DriveTrajectory(driveTrain, trajectory)
+         );*/
+    }
+    
+}
