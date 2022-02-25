4843e2e2d373f3a34bd664d25c47759fcc3357df
Fred Horch <fred.horch@gmail.com>
2022-02-25 11:48:23 -0500

    coordinate system

--- src/main/java/org/frc5687/rapidreact/commands/auto/AutoZeroBall.java
+++ src/main/java/org/frc5687/rapidreact/commands/auto/AutoZeroBall.java
@@ -3,6 +3,7 @@ package org.frc5687.rapidreact.commands.auto;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 import org.frc5687.rapidreact.RobotContainer;
+import org.frc5687.rapidreact.Constants;
 
 /**
  * Taxi out of tarmac.
@@ -16,17 +17,28 @@ public class AutoZeroBall extends SequentialCommandGroup {
      */
     public AutoZeroBall(RobotContainer robot) {
 
-        // Destination A
-        double xPos = -1.0;
-        double yPos = -1.0;
-        double theta = 0.0;
-        double omega = -0.5;
-        double velocity = 1.0;
-        DriveToPose driveToA = new DriveToPose(
-            robot.driveTrain, xPos, yPos, theta, omega, velocity);
+        double xPos;
+        double yPos;
+        double theta;
+        double omega;
+        double velocity;
+
+        // Choose destination depending on starting position
+        switch(Constants.Auto.AUTO_POSITION) {
+            case OBT_LEFT:
+            case OBT_RIGHT:
+            case TBT_LEFT:
+            case TBT_RIGHT:
+            default:
+                xPos = -1.0;
+                yPos = -1.0;
+                theta = 0.0;
+                omega = -0.5;
+                velocity = 1.0;    
+        }
         
         addCommands(
-            driveToA
+            new DriveToPose(robot.driveTrain, xPos, yPos, theta, omega, velocity)
         );
     }
 
